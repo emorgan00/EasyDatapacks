72461ddad1cb643c186713588991309c1a6023b6
emorgan00 <emorgan00@stuy.edu>
2019-06-20 19:22:02 -0400

    created a simple reference system

--- entity.py
+++ entity.py
@@ -1,13 +1,5 @@
-next_id = 0
+def select_entity(tag):
+	return '@e[tag=%s]' % tag
 
-class Entity:
-
-	def __init__(this, query):
-		this.query = query
-		this.id = next_id
-
-	def assignation(this):
-		return 'scoreboard players set %s id %s' % (this.selector(), this.id)
-
-	def selector(this):
-		return '@e[scores={id=%i}]' % this.id
+def clear_tag(tag):
+	return 'tag @e remove %s' % tag
