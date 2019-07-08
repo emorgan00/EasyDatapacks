36e47a785bff8efd634b7ff80b6dbe22ba6f1522
coolreader18 <33094578+coolreader18@users.noreply.github.com>
2019-07-08 10:55:31 -0500

    Only write __main__.py if egg

--- __main__.py
+++
@@ -1,4 +0,0 @@
-from datapack.cli import run
-
-if __name__ == "__main__":
-    run()
--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
 from setuptools import setup
-import sys
+import sys, os
 
 try:
     import py2exe
 except ImportError:
     pass
 
+
+egg_dist = "bdist_egg" in sys.argv
+if egg_dist:
+    main_file = os.path.realpath(os.path.join(__file__, "../__main__.py"))
+    with open(main_file, "w") as f:
+        f.write(
+            """
+from datapack.cli import run;
+if __name__ == "__main__":
+    run()
+"""
+        )
 
 setup(
     name="EasyDatapacks",
@@ -18,5 +30,8 @@ setup(
     package_dir={"datapack": "src"},
     packages=["datapack"],
     entry_points={"console_scripts": ["datapack = datapack.cli:run"]},
-    py_modules=["__main__"] if "bdist_egg" in sys.argv else [],
+    py_modules=["__main__"] if egg_dist else [],
 )
+
+if egg_dist:
+    os.remove(main_file)
